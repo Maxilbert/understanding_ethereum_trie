2e867a7a0e4d4b955d07041d5e2e22a572c8b237
Maxilbert <Maxilbert@users.noreply.github.com>
2018-11-24 18:35:04 -0500

    Update ex1 to support Python3 and new pyethereum

--- exercises/ex1.py
+++ exercises/ex1.py
@@ -1,11 +1,15 @@
-import sys
-sys.path.append('src')
-import trie, utils, rlp
+from ethereum import trie
+from ethereum.db import DB
+import codecs
 
-#initialize trie
-state = trie.Trie('triedb', trie.BLANK_ROOT)
-state.update('\x01\x01\x02', rlp.encode(['hello']))
-print 'root hash', state.root_hash.encode('hex')
+
+triedb = DB()
+state = trie.Trie(triedb, trie.BLANK_ROOT)
+print('root hash', state.root_hash)
+print('root hash', codecs.encode(state.root_hash, 'hex'))
+state.update(b'\x01\x02\x03', b'\x01\x02\x03')
+print('root hash', state.root_hash)
+print('root hash', codecs.encode(state.root_hash, 'hex'))
 k, v = state.root_node
-print 'root node:', [k, v]
-print 'hp encoded key, in hex:', k.encode('hex')
+print('root node:', [k, v])
+print('hp encoded key, in hex:', k)
