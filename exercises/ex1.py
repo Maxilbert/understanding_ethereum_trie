0ed3189aec8f5663fbf47c33d11273f5d7ff8804
Maxilbert <Maxilbert@users.noreply.github.com>
2018-11-24 18:38:25 -0500

    clean the code of ex1

--- exercises/ex1.py
+++ exercises/ex1.py
@@ -1,15 +1,15 @@
-from ethereum import trie
+from ethereum.trie import Trie, BLANK_ROOT
 from ethereum.db import DB
 import codecs
 
 
-triedb = DB()
-state = trie.Trie(triedb, trie.BLANK_ROOT)
-print('root hash', state.root_hash)
-print('root hash', codecs.encode(state.root_hash, 'hex'))
-state.update(b'\x01\x02\x03', b'\x01\x02\x03')
-print('root hash', state.root_hash)
-print('root hash', codecs.encode(state.root_hash, 'hex'))
-k, v = state.root_node
+db = DB()
+trie = Trie(db, BLANK_ROOT)
+print('root hash', trie.root_hash)
+print('root hash', codecs.encode(trie.root_hash, 'hex'))
+trie.update(b'\x01\x02\x03', b'\x01\x02\x03')
+print('root hash', trie.root_hash)
+print('root hash', codecs.encode(trie.root_hash, 'hex'))
+k, v = trie.root_node
 print('root node:', [k, v])
 print('hp encoded key, in hex:', k)
